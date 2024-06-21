f26ea855ae3950a761b6869c14d34321e11b33d7
Daniel Malcher <45374834+dbmalcher@users.noreply.github.com>
2024-06-21 09:14:23 -0400

    Create a Product Page

--- src/views/ProductsPage.vue
+++ src/views/ProductsPage.vue
@@ -1,15 +1,61 @@
 <template>
-    <h1>This is the Products Page</h1>
+  <div id="page-wrap">
+    <div class="grid-wrap">
+      <div v-for="product in products"
+      class="product-item"
+      :key = "product.id">
+        <img :src="product.imageUrl"/>
+        <h3 class="product-name">{{product.name}}</h3>
+        <p class="product-price">${{product.price}}</p>
+        <router-link
+          :to="'/products/' + product.id"
+        >
+        <button>View Details</button>
+      </router-link>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script>
-export default{
-    name: 'ProductsPage',
-}
+<script setup>
+import {products} from '../fake-data.js'
 </script>
 
 <style scoped>
- h1 {
-    color:red;
- }
+  .grid-wrap {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    margin-top: 16px;
+  }
+
+  .product-item {
+    align-items: center;
+    border-radius: 8px;
+    box-shadow: 0px 2px 5px #888;
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 2%;
+    padding: 20px;
+    position: relative;
+    width: 32%;
+  }
+
+  .product-name {
+    margin-bottom: 0;
+  }
+
+  img {
+    height: 200px;
+    width: 200px;
+  }
+
+  a {
+    width: 100%;
+  }
+
+  button {
+    width: 100%;
+  }
 </style>
+
